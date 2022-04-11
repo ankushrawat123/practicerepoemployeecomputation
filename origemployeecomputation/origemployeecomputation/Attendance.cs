96da5f0a152024eebbcfc6ff5258acd630930e9b
ankushrawat123 <ankushrwt344@gmail.com>
2022-04-11 10:24:23 +0530

    uploading attendance file

---
+++ origemployeecomputation/origemployeecomputation/Attendance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace origemployeecomputation
+{
+    internal class Attendance
+    {
+        public static void attendancemethod()
+        {
+
+
+            Random obj = new Random();
+            int num = obj.Next(0, 2);
+            if (num == 1)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+        }
+    }
+}
